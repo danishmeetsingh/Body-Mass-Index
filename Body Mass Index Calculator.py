c4281fb99525103584c31799c69c77597c3660b2
danishmeetsingh <saggudanish88@gmail.com>
2022-11-27 08:18:05 +0530

    Update Body Mass Index Calculator.py
    Now, you can also check in which category of BMI do you belong

--- Body Mass Index Calculator.py
+++ Body Mass Index Calculator.py
@@ -1,6 +1,16 @@
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 
-BMI_value= float(weight) / float(height) ** 2
-BMI = round(BMI_value, 3)
-print(str(BMI) + " kg/m²")
+BMI = float(weight) / float(height) ** 2
+
+if BMI < 18.5:
+  print(f"Your BMI is {BMI}. You are Underweight")
+elif BMI < 25:
+  print(f"Your BMI is {BMI}. You have a Normal Weight")
+elif BMI < 30:
+  print(f"Your BMI is {BMI}. You are Overweight")
+elif BMI < 35:
+  print(f"Your BMI is {BMI}. You are Obese")
+else:
+  print(f"Your BMI is {BMI}. You are Clinically Obese")
+  
